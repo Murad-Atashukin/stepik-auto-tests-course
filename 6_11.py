e4984f9cf69adaac3570fb28b6a4b9208d7d6333
Murad-Atashukin <aslan0799.am@gmail.com>
2022-01-29 17:47:53 +0300

    lesson 2.6, task 11

--- 6_11.py
+++ 6_11.py
@@ -3,20 +3,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ex
 from selenium.webdriver.common.by import By
 import time
-from math import*
+from math import *
+
 def calc(x):
     return log(abs(12 * sin(x)))
+
 link = "http://suninjuly.github.io/explicit_wait2.html"
 browser = webdriver.Chrome()
 browser.get(link)
+
 try:
     WebDriverWait(browser, 12).until(
     ex.text_to_be_present_in_element((By.ID, "price"), "100")
     )
     button = browser.find_element_by_id("book").click()
-    x_element = browser.find_element_by_id("input_value")
-    x = int(x_element.text)
-    _calc = calc(x)
+    x_element = int(browser.find_element_by_id("input_value").text)
+
+    _calc = calc(x_element)
     input2 = browser.find_element_by_id("answer").send_keys(str(_calc))
     button1 = browser.find_element_by_id("solve").click()
 finally:
